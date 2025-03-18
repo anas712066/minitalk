0da9877b71063fd3e6bdd6dee3135e09705c9e44
Muhammad Anas <anasabid607@gmail.com>
2025-03-18 22:08:47 +0100

    fractal

--- minitalk.h
+++ minitalk.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mumajeed <mumajeed@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 22:07:56 by mumajeed          #+#    #+#             */
+/*   Updated: 2025/03/18 22:07:58 by mumajeed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
@@ -10,8 +22,8 @@
 # include <limits.h>
 # include <stdbool.h>
 
-#define BUSY	0
-#define READY	1
+# define BUSY	0
+# define READY	1
 
 void	Signal(int sig, void *handler, bool use_siginfo);
 void	Kill(pid_t pid, int signum);
